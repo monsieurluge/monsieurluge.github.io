0290c2f624948e0e85659a93a87e757e50eebf76
monsieurluge <monsieurluge@gmail.com>
2019-07-02 12:07:17 +0200

    init the main vue

---
+++ cpc-mariomaker/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el: '#app',
+  data () {
+    return {
+      title: 'Mario Maker - Les niveaux créés par la communauté CanardPC',
+      tags: [ 'test', 'mini', 'expert', 'puzzle' ]
+    }
+  }
+})
