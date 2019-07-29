3804f57fd3e11deaed71b6380514dba78fa9a85c
monsieurluge <monsieurluge@gmail.com>
2019-07-29 23:31:58 +0200

    level card tooltip styling

--- cpc-mariomaker/components/level-card.vue.js
+++ cpc-mariomaker/components/level-card.vue.js
@@ -1,14 +1,23 @@
 Vue.component('LevelCard', {
   template: `
     <div class="level-card">
-      <h2>{{ code }}</h2>
+      <div class="tooltip" v-bind:data-tooltip="createdBy">
+        <div class="level-card-content">
+          <h2>{{ code }}</h2>
 
-      <p class="level-card-infos"><span class="title">{{ title }}</span>, par <span class="by">{{ by }}</by></p>
+          <p class="level-card-title">{{ title }}</p>
+        </div>
+      </div>
     </div>
   `,
   props: {
     code: { type: String, required: true },
     by: { type: String, required: true },
     title: { type: String, required: true }
+  },
+  computed: {
+    createdBy () {
+      return `par @${this.by}`
+    }
   }
 })
