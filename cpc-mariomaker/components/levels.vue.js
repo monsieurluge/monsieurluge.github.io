b58ca7d9145858bea1069808b8a48c254939d51b
monsieurluge <monsieurluge@gmail.com>
2019-07-30 18:29:29 +0200

    filter the levels

--- cpc-mariomaker/components/levels.vue.js
+++ cpc-mariomaker/components/levels.vue.js
@@ -1,14 +1,40 @@
 Vue.component('Levels', {
   template: `
   <div class="levels">
-    <LevelsSelectors v-bind="totals" @selected="filterLevels" />
+    <LevelsSelectors v-bind="totals" @selected="displayLevelsUsing" />
 
-    <LevelCard v-for="level in levels" :key="level.id" v-bind="level" />
+    <LevelCard v-for="level in filteredLevels" :key="level.id" v-bind="level" />
   </div>
   `,
   data () {
     return {
-      totals: {
+      activeFilter: 'all',
+      filters: [
+        { name: 'all', method: () => true },
+        { name: 'favs', method: level => this.favs.includes(level.id) },
+        { name: 'mine', method: level => this.mine.includes(level.id) },
+        { name: 'todo', method: level => !this.done.includes(level.id) },
+      ]
+    }
+  },
+  props: {
+    done: { type: Array, required: false, default: [1, 2, 3] },
+    favs: { type: Array, required: false, default: [2, 3, 1] },
+    levels: { type: Array, required: true },
+    mine: { type: Array, required: false, default: [6, 7] }
+  },
+  computed: {
+    filteredLevels () {
+      return this.levels.filter(this.filteringMethod)
+    },
+    filteringMethod () {
+      const fallback = { name: this.activeFilter, method: () => true }
+      return [...this.filters, fallback]
+        .find(filter => filter.name === this.activeFilter)
+        .method
+    },
+    totals () {
+      return {
         total: 34,
         favs: 5,
         mine: 2,
@@ -16,12 +42,9 @@ Vue.component('Levels', {
       }
     }
   },
-  props: {
-    levels: { type: Array, required: true }
-  },
   methods: {
-    filterLevels (filter) {
-      console.log('FIXME: filter by', filter)
+    displayLevelsUsing (filter) {
+      this.activeFilter = filter
     }
   }
 })
