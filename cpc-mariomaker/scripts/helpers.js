663100376435f93a40750edb6665f99513923566
monsieurluge <monsieurluge@gmail.com>
2019-08-01 14:35:46 +0200

    new helper functions: add user infos to levels list

--- cpc-mariomaker/scripts/helpers.js
+++ cpc-mariomaker/scripts/helpers.js
@@ -1,1 +1,27 @@
 const pipe = (...functions) => param => functions.reduce((result, func) => func(result), param)
+
+const addUserInfos = userInfos => {
+  return pipe(
+    addMineInfo(userInfos),
+    addFavInfo(userInfos),
+    addDoneInfo(userInfos),
+  )
+}
+
+const addDoneInfo = userInfos => level => {
+  return userInfos.done.includes(level.id)
+    ? Object.assign(level, { done: true })
+    : level
+}
+
+const addFavInfo = userInfos => level => {
+  return userInfos.favs.includes(level.id)
+    ? Object.assign(level, { fav: true })
+    : level
+}
+
+const addMineInfo = userInfos => level => {
+  return userInfos.mine.includes(level.id)
+    ? Object.assign(level, { mine: true })
+    : level
+}
