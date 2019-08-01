8a0189d0c09b29458e6594c6a783b79736fc14d7
monsieurluge <monsieurluge@gmail.com>
2019-08-01 15:47:59 +0200

    add the level icons

--- cpc-mariomaker/components/level-card.vue.js
+++ cpc-mariomaker/components/level-card.vue.js
@@ -3,7 +3,10 @@ Vue.component('LevelCard', {
     <div class="level-card">
       <div class="tooltip" v-bind:data-tooltip="createdBy">
         <div class="level-card-content">
-          <h2>{{ code }}</h2>
+          <div class="level-card-first-line">
+            <h2>{{ code }}</h2>
+            <LevelIcons class="level-card-icons" v-bind="userInfos"/>
+          </div>
 
           <p class="level-card-title">{{ title }}</p>
         </div>
@@ -13,11 +16,21 @@ Vue.component('LevelCard', {
   props: {
     code: { type: String, required: true },
     by: { type: String, required: true },
+    done: { type: Boolean, required: false, default: false },
+    fav: { type: Boolean, required: false, default: false },
+    mine: { type: Boolean, required: false, default: false },
     title: { type: String, required: true }
   },
   computed: {
     createdBy () {
       return `par @${this.by}`
+    },
+    userInfos () {
+      return {
+        done: this.done,
+        fav: this.fav,
+        mine: this.mine
+      }
     }
   }
 })
