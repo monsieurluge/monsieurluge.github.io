d642aab570b53aaa972b902d4e954800a8666fd3
monsieurluge <monsieurluge@gmail.com>
2019-08-01 15:46:06 +0200

    create the "icon with tooltip" component

---
+++ cpc-mariomaker/components/icon-tooltip.vue.js
@@ -0,0 +1,29 @@
+Vue.component('IconTooltip', {
+  template: `
+    <div class="tooltip" v-bind:data-tooltip="label">
+      <div class="icon-toggle-wrapper" v-bind:class="isActive ? 'active' : ''">
+        <div class="icon-toggle-icon" v-bind:class="iconName" @click="onClick" />
+        <div class="icon-toggle-badge" v-if="badge > 0" >{{ badge }}</div>
+      </div>
+    </div>
+  `,
+  props: {
+    active: { type: Boolean, required: false, default: false },
+    icon: { type: String, required: false, default: 'smile'},
+    label: { type: String, required: true },
+    badge: { type: Number, required: false, default: 0 }
+  },
+  computed: {
+    iconName () {
+      return `icon icon-${this.icon}`
+    },
+    isActive () {
+      return this.active
+    }
+  },
+  methods: {
+    onClick () {
+      this.$emit('clicked')
+    }
+  }
+})
